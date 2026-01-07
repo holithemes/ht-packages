b385d3f9686488872f32eb4ad23bb45896f93360
“harsha-ht-code” <harsha@holithemes.com>
2026-01-07 12:17:05 +0530

    deleted the packeges

--- packages/ht-packages/index.js
+++
@@ -1,9 +0,0 @@
-import { validateConfig, mergeConfigs, getPackageInfo } from '@holithemes/shared-utils';
-
-export { validateConfig, mergeConfigs, getPackageInfo } from '@holithemes/shared-utils';
-
-export default {
-  validateConfig,
-  mergeConfigs,
-  getPackageInfo
-};
